49bc196a240badfa117e885e440f7417e89e04f6
Dave Fennessey <dave@dfennessey.com>
2024-01-08 10:43:08 +0100

    flask deploy

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,5 @@ WORKDIR $APP_HOME
 COPY . $APP_HOME/app
 
 EXPOSE 8080
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
+#CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
+CMD ["gunicorn" ,"--bind", ":8080", "app.main:app"]
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
 
-@app.get("/", tags=["root"])
-async def root():
-    return {"message": "Hello World"}
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
