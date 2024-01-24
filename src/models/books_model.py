e627b0550e1e459f0d041e755346adb444cc4e88
Dave Fennessey <dave@dfennessey.com>
2024-01-24 07:45:22 +0100

    switching from pandas to polars

--- src/models/books_model.py
+++ src/models/books_model.py
@@ -1,7 +1,9 @@
 import logging
 from datetime import datetime
+from typing import Dict, Set
 
 import pandas
+import polars as pl
 from pydantic import BaseModel, ConfigDict
 
 logger = logging.getLogger(__name__)
@@ -9,15 +11,22 @@ logger = logging.getLogger(__name__)
 
 class BooksModel(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
-    books: pandas.DataFrame
+    books: pl.DataFrame
+    genre_inverted_index: Dict[str, Set[str]]
 
     @classmethod
     def load_from_pickle(cls, path):
-        with open(f"{path}/books.pkl", "rb") as f:
+        with open(f"{path}/genres_inverted_index.pkl", "rb") as f:
             start = datetime.now()
-            book_df = pandas.read_pickle(f)
-            book_df["genres"] = book_df["genres"].apply(set)
+            inverted_index = pandas.read_pickle(f)
             logger.info(
-                f"Loaded book_df in {(datetime.now() - start).microseconds / 1000} milliseconds"
+                f"Loaded inverted_index in {(datetime.now() - start).microseconds / 1000} milliseconds"
             )
-        return cls(books=book_df)
+
+        start = datetime.now()
+        book_df = pl.read_parquet(f"{path}/books.parquet")
+        logger.info(
+            f"Loaded book_df in {(datetime.now() - start).microseconds / 1000} milliseconds"
+        )
+
+        return cls(books=book_df, genre_inverted_index=inverted_index)
--- src/service/profile_service.py
+++ src/service/profile_service.py
@@ -1,7 +1,8 @@
 import logging
+from typing import Dict, List, Set
 
 import numpy as np
-import pandas as pd
+import polars as pl
 from flask import current_app
 
 from src.models.books_model import BooksModel
@@ -11,58 +12,73 @@ logger = logging.getLogger(__name__)
 
 
 # Add a function to identify items with any of the specified genres
-def get_genre_filtered_items(book_df, genre_list):
+def get_genre_filtered_items(
+    inverted_index: Dict[str, Set[str]], genre_list: List[str]
+):
+    if not genre_list:
+        return set()
+
     genre_set = set(genre_list)
-    mask = book_df["genres"].map(lambda genres: genre_set.issubset(genres))
-    return book_df[mask]
+    try:
+        common_work_ids = set.intersection(
+            *(inverted_index[genre] for genre in genre_set)
+        )
+    except KeyError as e:
+        logger.warning(f"Genre not found in inverted index: {e}")
+        return set()
+    return common_work_ids
 
 
 def get_top_n_recommendations(
-    user_id: str, model: ProfileModel, book_df: BooksModel, genre_list=None, n=10
+    user_id: str,
+    profile_model: ProfileModel,
+    book_model: BooksModel,
+    genre_list=None,
+    n=10,
 ):
     # Map the user ID to the internal user ID used by LightFM
-    internal_user_id = model.user_id_map.get(user_id)
+    internal_user_id = profile_model.user_id_map.get(user_id)
     if internal_user_id is None:
         raise ValueError("User ID not found in the training set.")
 
     # Get the interactions mask for the user (items the user has already interacted with)
-    interactions_mask = model.interactions[internal_user_id].astype(bool).toarray()[0]
+    interactions_mask = (
+        profile_model.interactions[internal_user_id].astype(bool).toarray()[0]
+    )
 
     # Generate predictions across all items for this user
-    scores = model.model.predict(
+    scores = profile_model.model.predict(
         user_ids=internal_user_id,
-        item_ids=np.arange(model.item_features_matrix.shape[0]),
-        item_features=model.item_features_matrix,
+        item_ids=np.arange(profile_model.item_features_matrix.shape[0]),
+        item_features=profile_model.item_features_matrix,
     )
 
     # Mask out already interacted items to avoid recommending them
     scores = np.where(interactions_mask, -np.inf, scores)
 
     if genre_list:
-        genre_filtered_items = get_genre_filtered_items(book_df, genre_list)
-        genre_filtered_item_ids = genre_filtered_items.work_id.tolist()
+        genre_filtered_item_ids = get_genre_filtered_items(
+            book_model.genre_inverted_index, genre_list
+        )
 
-        # Get the internal item IDs for the filtered items
         genre_filtered_internal_item_ids = [
-            model.item_id_map[work_id]
+            profile_model.item_id_map[work_id]
             for work_id in genre_filtered_item_ids
-            if work_id in model.item_id_map
+            if work_id in profile_model.item_id_map
         ]
 
-        # Create a boolean mask for all items, where True indicates the item matches the genre filter
         genre_mask = np.isin(
-            np.arange(model.item_features_matrix.shape[0]),
+            np.arange(profile_model.item_features_matrix.shape[0]),
             genre_filtered_internal_item_ids,
         )
 
-        # Apply the genre mask to the scores, setting scores of non-matching items to -np.inf
         scores = np.where(genre_mask, scores, -np.inf)
 
     # Rank items by score. Use argsort in descending order while avoiding recommending already interacted items
     top_item_indices = np.argsort(scores)[-n:]
 
     # Translate back to original item IDs and get their scores
-    reverse_item_id_map = {v: k for k, v in model.item_id_map.items()}
+    reverse_item_id_map = {v: k for k, v in profile_model.item_id_map.items()}
     top_items_scores = [
         (reverse_item_id_map[idx], scores[idx]) for idx in reversed(top_item_indices)
     ]
@@ -77,18 +93,24 @@ def get_top_n_recommendations(
 
 def get_predictions(user_id, genre_list=None, n=10):
     model = current_app.profile_model
-    book_df = current_app.book_model.books
+    book_df = current_app.book_model
 
     try:
         top_work_ids, top_scores = get_top_n_recommendations(
             user_id, model, book_df, genre_list=genre_list, n=n
         )
-        top_items_df = pd.DataFrame({"work_id": top_work_ids, "score": top_scores})
-        # Merge with the book_df to get the book titles
-        top_items_df = top_items_df.merge(
-            book_df[["work_id", "book_title"]], on="work_id"
+
+        top_work_ids_np = np.array(top_work_ids)
+        top_scores_np = np.array(top_scores)
+        top_items_df = pl.DataFrame(
+            {"work_id": top_work_ids_np, "score": top_scores_np}
         )
-        recommendations = top_items_df.to_dict(orient="records")
-        return recommendations
+
+        top_items_df = top_items_df.join(
+            book_df.books.select(["work_id", "book_title"]), on="work_id"
+        )
+
+        return top_items_df.to_dicts()
+
     except ValueError as e:
-        logger.info(e)
+        logger.error(e)
